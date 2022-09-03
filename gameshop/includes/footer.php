16e929788a56fa9f2550a2a76038fad2affa9766
Joy097 <69787432+Joy097@users.noreply.github.com>
2022-09-03 23:23:51 +0600

    Add files via upload

---
+++ gameshop/includes/footer.php
@@ -0,0 +1,82 @@
+
+<footer id="footer" class="footer color-bg" >
+  <div class="links-social">
+    <div class="container-fluid">
+      <div class="row upper1" style="background-color: #2C3639;font-size: 20px;">
+        <div class="col-sm-6 col-md-4">
+          <div class="contact-timing">
+            <div class="module-heading">
+              <h3 class="module-title" style="color: white;">Available :</h3>
+            </div>
+            <div class="module-body outer-top-xs">
+              <div class="table-responsive">
+                <table class="table" style="color: white;">
+                  <tbody>
+                    <tr><td><b>Sunday-Thursday:</b> 8am To 7pm</td></tr>
+                    <tr><td><b>Friday-Saturday:</b> Off Days</td></tr> 
+                    <tr><td> <b>Online Service:</b> 24/7!</td> </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4"  >
+          <div class="contact-information">
+            <div class="module-heading">
+              <h3 class="module-title" style="color: white; ">Contact</h3>
+            </div>
+            <div class="module-body outer-top-xs">
+              <ul class="toggle-footer">
+                <li class="media">
+                  <div class="pull-left">
+                    <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                  </div>
+                  <div class="media-body" style="color: white;">
+                    <p>Gulshan,Dhaka</p>
+                  </div>
+                </li>
+                <li class="media">
+                  <div class="pull-left">
+                    <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                  </div>
+                  <div class="media-body" style="color: white;">
+                    <p>+8801572062193</p>
+                  </div>
+                </li>
+                <li class="media">
+                  <div class="pull-left">
+                    <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                  </div>
+                  <div class="media-body">
+                    <span><a href="#" style="color: white;">dread077@gmail.com</a></span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row  lower1" style="background-color: black;" >
+        <div class="col-sm-5 col-md-4">
+          <a href="index.php" ><img style="width: 130px;" src="img/lo.png"></a>
+        </div>
+        <div class="col-sm-5 col-md-6">
+          <p class="text-center copyright" style="color: white;">Copyright <i class="fa fa-copyright"></i> Dread-2022 | Designed and Developed By Shihab Rumee Chowdhury</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</footer>
+
+    
